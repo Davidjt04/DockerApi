81c9232feaa3674b9c34bd0b18fd1ab5d1ceccdc
Davidjt04 <dajantor@gmail.com>
2024-11-26 08:30:54 +0100

    construccion de API

--- Back/api.php
+++ Back/api.php
@@ -1,31 +1,41 @@
 <?php
-header("Content-Type: application/json");
+/*codigo que manda la response */
+//cogemos el REQUEST_METHOD pertinente
+include '../controladores/ControlIngrediente.php';
+$request = $_SERVER['REQUEST_METHOD'];
+//Creo un objeto controlador que usare en el switch 
+$control = new controlIngrediente();
+switch ($request) {
+    case 'GET':
+        $cadena = $_GET['id'];
+        // var_dump($cadena);
+        // var_dump($cadena);
+        //Creo un objeto controlador
+        $tupla = $control->cogerDatos($cadena);
+        // var_dump($tupla);
+        // echo json_encode($tupla);
+        $jsonTupla = json_encode($tupla);
+        echo $jsonTupla;
+        break;
+    case 'POST':  
+        //me va a modificar y a crear el objeto 
+        $datos = json_decode(file_get_contents('php://input'),true);
+        //llamo al controlador para ya guardar en la base de datos 
+        json_encode($datos);
+       $control->meterDatos($datos);
+       
+        break;
+    case 'DELETE':
+        //Va a borrar los datos 
+        $datos = json_decode(file_get_contents('php://input'),true);
+        $id= $datos['id'];
+        // json_encode($datos);
+        $control->borrarDatos($id);
+        break;
 
-$dsn = 'mysql:host=db;dbname=mydb;charset=utf8';
-$username = 'root';
-$password = 'root';
-
-try {
-    $conn = new PDO($dsn, $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        // Obtener todos los productos
-        $stmt = $conn->query("SELECT * FROM kebab");
-        $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($productos);
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Crear un nuevo producto
-        $input = json_decode(file_get_contents('php://input'), true);
-        $stmt = $conn->prepare("INSERT INTO productos (idkebab, nombre, precio) VALUES (:idkebab, :nombre, :precio)");
-        $stmt->bindParam(':idkebab', $input['idkebab']);
-        $stmt->bindParam(':nombre', $input['nombre']);
-        $stmt->bindParam(':nombre', $input['nombre']);
-    $stmt->execute();
-        echo json_encode(['status' => 'kebab creado']);
-    }
-} catch (PDOException $e) {
-    echo json_encode(['error' => $e->getMessage()]);
+    default:
+        throw new Exception("Error al procesar la Request");
+        
 }
 
-?>  
+?>
